545d54717dc861816a64b33f7464c64b6779e259
Donnies Mathews <annmary8123@gmail.com>
2021-11-16 21:07:28 +0530

    logic with index starting from 0

--- Pattern3/src/academy/learnprogramming/Main.java
+++ Pattern3/src/academy/learnprogramming/Main.java
@@ -20,12 +20,22 @@ public class Main {
 //        }
 //    }
     //ANOTHER LOGIC
+//    static void pattern3(int n) {
+//        for (int row = 1; row <= n; row++) {
+//            for (int col = 1; col <= n-row+1; col++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println(" ");
+//        }
+//    }
+    //Also Another Logic
     static void pattern3(int n) {
-        for (int row = 1; row <= n; row++) {
-            for (int col = 1; col <= n-row+1; col++) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n-row; col++) {
                 System.out.print("* ");
             }
             System.out.println(" ");
         }
     }
+
 }
