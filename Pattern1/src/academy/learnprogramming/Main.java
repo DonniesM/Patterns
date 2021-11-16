adbf8161905dfcc929ea8d7289987c9b7d0891a8
Donnies Mathews <annmary8123@gmail.com>
2021-11-16 12:11:03 +0530

    added pattern1

---
+++ Pattern1/src/academy/learnprogramming/Main.java
@@ -0,0 +1,22 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows you want : ");
+        int n = sc.nextInt();
+        pattern1(n);
+    }
+    static void pattern1(int n){
+        for (int row = 0;row < n;row++){
+            for(int col = 0;col < n; col++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
