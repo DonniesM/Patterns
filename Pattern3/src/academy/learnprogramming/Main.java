7a833b1a968344f14c4ad99f8088bde301f1898c
Donnies Mathews <annmary8123@gmail.com>
2021-11-16 12:24:47 +0530

    added Pattern3 purely my logic

---
+++ Pattern3/src/academy/learnprogramming/Main.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows you want : ");
+        int n = sc.nextInt();
+        pattern3(n);
+    }
+    static void pattern3(int n){
+        for(int row = 0; row < n; row++){
+            for(int col = n-1; col >= row; col--){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
