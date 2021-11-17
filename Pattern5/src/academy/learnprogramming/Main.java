fda09a25d9120df39147defbac720442026d2eca
Donnies Mathews <annmary8123@gmail.com>
2021-11-17 11:26:36 +0530

    added - Pattern5 my version of code

---
+++ Pattern5/src/academy/learnprogramming/Main.java
@@ -0,0 +1,36 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements required : ");
+        int n = sc.nextInt();
+        pattern5(n);
+    }
+
+    static void pattern5(int n) {
+        int i = 1;
+
+        for (int row = 1; row <= 2 * n - 1; row++) {
+
+            if (row <= n) {
+                for (int col = 1; col <= row; col++) {
+                    System.out.print("* ");
+                }
+                System.out.println(" ");
+            } else {
+//                if (row >= n) {
+                    for (int col = 1; col <= n-i; col++) {
+                        System.out.print("* ");
+
+                    }
+                    i++;
+                    System.out.println(" ");
+//                }
+            }
+        }
+    }
+}
