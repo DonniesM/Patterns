141cba1d96218003e4e2c6faf2b5dea7328c6503
Donnies Mathews <annmary8123@gmail.com>
2021-11-23 10:55:47 +0530

    added - kk solution to Pattern17, solution even simpler
    instead of using spaces as the parameter for aligning rows and cols use cols for the other two

--- Pattern17/src/academy/learnprogramming/Main.java
+++ Pattern17/src/academy/learnprogramming/Main.java
@@ -6,32 +6,54 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n : ");
         int n = sc.nextInt();
-        pattern17(n);
+
+//        pattern17(n);
+        pattern17k(n);
+
     }
-    static void pattern17(int n){
-        for(int row = 1;row <= 2*n; row++){
-            int totalSpaces = (row<=n)?(n-row):row-n;
-            for(int s = 1; s <= totalSpaces; s++){
+
+    static void pattern17(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+            int totalSpaces = (row <= n) ? (n - row) : row - n;
+            for (int s = 1; s <= totalSpaces; s++) {
                 System.out.print("  ");
             }
-            if(row<=n){
-                for(int col = row; col >= 1; col--){
-                    System.out.print(col+" ");
+            if (row <= n) {
+                for (int col = row; col >= 1; col--) {
+                    System.out.print(col + " ");
                 }
-                for(int col1 = 2; col1 <= row; col1++){
-                    System.out.print(col1+" ");
+                for (int col1 = 2; col1 <= row; col1++) {
+                    System.out.print(col1 + " ");
                 }
-            }
-            else{
-                for(int col = 2*n-row; col>=1; col--){
-                    System.out.print(col+" ");
+            } else {
+                for (int col = 2 * n - row; col >= 1; col--) {
+                    System.out.print(col + " ");
                 }
-                for(int col1 = 2; col1 <= 2*n-row; col1++){
-                    System.out.print(col1+" ");
+                for (int col1 = 2; col1 <= 2 * n - row; col1++) {
+                    System.out.print(col1 + " ");
                 }
 
             }
+            System.out.println(" ");
+        }
+    }
+
+    static void pattern17k(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+            int totalColsInRows = (row <= n) ? row : (2 * n - row);
+            for (int s = 1; s <= n - totalColsInRows; s++) {
+                System.out.print("  ");
+            }
+
+            for (int col = totalColsInRows; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col1 = 2; col1 <= totalColsInRows; col1++) {
+                System.out.print(col1 + " ");
+            }
+
             System.out.println(" ");
         }
     }
