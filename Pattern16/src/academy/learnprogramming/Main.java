a072f539013d5ae07c84c7eeeb02804f4b2c7a17
Donnies Mathews <annmary8123@gmail.com>
2021-12-13 19:31:47 +0530

    added code to print Pascals triangle for a given value n custom

---
+++ Pattern16/src/academy/learnprogramming/Main.java
@@ -0,0 +1,34 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n : ");
+        int n = sc.nextInt();
+        System.out.println();
+        pattern16(n);
+    }
+    static void pattern16(int n){
+        int PascalTriangle[][] = new int[n][2*n];
+
+        for(int row = 0; row<n; row++){
+
+            for(int s=0;s<n-row-1;s++){
+                System.out.print(" ");
+            }
+            int colValue = 0;
+            originalCol:
+            for(int col = n-row;col<=2*n-n+row;col+=2){
+                int count = 0;
+                colValue = ((col!=n-row)&&(col!=2*n-n+row))?PascalTriangle[row-1][col-1]+PascalTriangle[row-1][col+1]:1;
+                PascalTriangle[row][col]=colValue;
+                System.out.print(PascalTriangle[row][col]+" ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+}
