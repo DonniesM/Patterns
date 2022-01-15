1c314fef1f66c7a2019a11207a3d490243655814
Donnies Mathews <annmary8123@gmail.com>
2022-01-15 14:37:03 +0530

    star M pattern
     second

---
+++ Pattern23Second/src/academy/learnprogramming/Main.java
@@ -0,0 +1,51 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n : ");
+        int n = sc.nextInt();
+        pattern23Second(n);
+    }
+
+    static void pattern23Second(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                if (col != n - row + 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+
+
+            if (row != 1) {
+                for (int space = 1; space <= row - 2; space++) {
+                    System.out.print(" ");
+                }
+                for (int col = row; col < (2 * n); col++) {
+
+                    if (col != row && col != 2 * n - row) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("*");
+                    }
+                }
+            }
+            else {
+                System.out.printf("%7s"," ");
+            }
+            for (int col = 1; col <= n; col++) {
+                if (col != row) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
